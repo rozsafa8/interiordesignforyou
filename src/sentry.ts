f054cabfd142942c7e49f81882419d505febce8a
István Kiss <kissisti98@outlook.com>
2026-02-11 21:15:08 +0100

    sentry config

---
+++ src/sentry.ts
@@ -0,0 +1,20 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://2034216ae437759d64e12730415d5736@o4508751275098112.ingest.de.sentry.io/4510869102985296",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [/^https:\/\/rozsafa8\.hu/],
+  // Session Replay
+  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  sendDefaultPii: true,
+});
