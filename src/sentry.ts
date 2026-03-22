4f9d1d40e006e82d69ca3244791c8b9864b41f5e
István Kiss <kissisti98@outlook.com>
2026-03-22 10:37:27 +0100

    fix sentry url

--- src/sentry.ts
+++ src/sentry.ts
@@ -13,7 +13,7 @@ export const initSentry = () => {
     // Tracing
     tracesSampleRate: 1.0, //  Capture 100% of the transactions
     // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-    tracePropagationTargets: [/^https:\/\/rozsafa8\.hu/],
+    tracePropagationTargets: [/^https:\/\/interiordesignforyou\.com/],
     // Session Replay
     replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
